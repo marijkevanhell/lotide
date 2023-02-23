1716bde1f8854bd3ca8e56ad32492e09acf01f0b
marijkevanhell <jukevanhell@gmail.com>
2023-02-23 20:13:11 +0000

    used assertEqual function to return head only

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(inputArray){
+  const firstElement = inputArray[0]
+  return firstElement
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), undefined);
