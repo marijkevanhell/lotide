2455ecb3667eef5327bc4391e10bda7d9469afc5
marijkevanhell <jukevanhell@gmail.com>
2023-02-23 22:07:30 +0000

    cleaned up with ESLint

--- tail.js
+++ tail.js
@@ -5,21 +5,21 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-const tail = function(inputArray){
-  const tailArray = []
-  for (let x = 0; x < inputArray.length; x++){
-    if (x === 0){
+const tail = function(inputArray) {
+  const tailArray = [];
+  for (let x = 0; x < inputArray.length; x++) {
+    if (x === 0) {
       //Do nothing//
     } else {
-      console.log("value of x", x)
-      tailArray.push(inputArray[x])
-      console.log("value of tailArray", tailArray)
+      console.log("value of x", x);
+      tailArray.push(inputArray[x]);
+      console.log("value of tailArray", tailArray);
     }
   }
-  return tailArray
-}
+  return tailArray;
+};
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
