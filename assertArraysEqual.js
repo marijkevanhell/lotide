ad3acdc4455b8017b05157a5f130023e3544bf06
marijkevanhell <jukevanhell@gmail.com>
2023-02-25 20:30:16 -0800

    created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arr1, arr2) {
+  let x = arr1.length
+  let y = arr2.length
+  if (x != y) {
+    console.log("❌❌❌Assertion Failed: true");
+    return
+  }
+  let arrays_same = true
+  for (let x = 0; x < arr1.length; x++) {
+    if (arr1[x] !== arr2[x]) {
+      arrays_same = false
+      console.log("❌❌❌Assertion Failed: true");
+      return
+    }
+  }
+  console.log("✅✅✅Assertion Passed: true");
+  return
+};
+
+eqArrays([1, 2, 3], [1, 2, 3])
+eqArrays([1, 2, 3], [1, 2, 4])
