35149064158b2c666af9a689716f59849dc9d77b
marijkevanhell <jukevanhell@gmail.com>
2023-03-03 01:22:22 +0000

    refactored and cleaned code with eslint

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,21 @@
-const assertArraysEquals = function(arr1, arr2) {
-  let x = arr1.length
-  let y = arr2.length
-  if (x != y) {
-    console.log("❌❌❌Assertion Failed: true");
-    return
-  }
-  let arrays_same = true
+const eqArrays = function(arr1, arr2) {
+  let length1 = arr1.length;
+  let length2 = arr2.length;
+  if (length1 !== length2) return false;
   for (let x = 0; x < arr1.length; x++) {
-    if (arr1[x] !== arr2[x]) {
-      arrays_same = false
-      console.log("❌❌❌Assertion Failed: true");
-      return
-    }
+    if (arr1[x] !== arr2[x]) return false;
   }
-  console.log("✅✅✅Assertion Passed: true");
-  return
+  return true;
 };
 
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//test code
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2], [1, 4]);
