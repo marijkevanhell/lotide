3d434d38bdfd01655f74c7b5ce78e33d55aed6f7
marijkevanhell <jukevanhell@gmail.com>
2023-03-15 04:33:35 +0000

    refactored test files

---
+++ test/middleTest.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+//returns 1 element
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+//returns 2 elements
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
